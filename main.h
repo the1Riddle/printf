70eaf463e6cf208bc08f80fc43c2f81ff95e4c41
ElvisMalsa <elvisotienomboya@gmail.com>
2023-04-28 11:40:42 -0700

    the main header file

--- main.h
+++ main.h
@@ -7,20 +7,31 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR (va_list arg);
 
 
 /**
- * struct idfierStruct - structure definition of a printTypeStruct
+ * struct typeStruct - structure definition of a printTypeStruct
  * @type: type
  * @printer: function to print
  */
-typedef struct idfierStruct
+typedef struct typeStruct
 {
 	char *type;
 	int (*printer)(va_list);
-} idfierStruct;
+} typeStruct;
 
 #endif
